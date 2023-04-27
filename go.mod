f07b48e0f6354f0af00ee21549c49a847edc89c7
chris <chrisllamas2000@gmail.com>
2023-04-27 16:13:43 +0000

    First commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/chllamas/ezw_api
+
+go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "database/sql"
+    "log"
+    "os"
+
+     _ "github.com/go-sql-driver/mysql"
+     _ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+    db, err := sql.Open("mysql", os.Getenv("DSN"))
+    if err != nil {
+        log.Fatalf("failed to connect: %v", err)
+    }
+    defer db.Close()
+
+    if err := db.Ping(); err != nil {
+        log.Fatalf("failed to ping: %v", err)
+    }
+
+    log.Println("Successfully connected to PlanetScale!")
+}
+
