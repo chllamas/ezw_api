bb23f614cc5d8c08bec48a5600308c98173901af
chris <chrisllamas2000@gmail.com>
2023-05-11 20:17:14 +0000

    working with db stuff now

--- db/init.go
+++ db/init.go
@@ -8,8 +8,8 @@ import (
 	"gorm.io/gorm"
 )
 
-var database *gorm.DB
 var secretKey string
+var database *gorm.DB = nil
 var usernameSanitizer = regexp.MustCompile(`^[a-zA-Z0-9](?:[a-zA-Z0-9._]{1,30}[a-zA-Z0-9])?$`)
 var passwordSanitizer = regexp.MustCompile(`^[a-zA-Z0-9!@#$%^&*?]{8,128}$`)
 
@@ -22,9 +22,9 @@ type UserLogin struct {
 }
 
 type User struct {
-    Username    string     `gorm:"primaryKey"`
-    Hash        [32]byte
-    Salt        [32]byte
+    Username    string      `gorm:"type:varchar(32);primaryKey"`
+    Hash        [32]byte    `gorm:"type:binary(32);not null"`
+    Salt        [32]byte    `gorm:"type:binary(32);not null"`
 }
 
 func TODO() error {
@@ -32,12 +32,16 @@ func TODO() error {
 }
 
 func Init(dsn string, sk string) {
+    if database != nil {
+        panic("Datbase init can only be called once!")
+    }
     var err error
     secretKey = sk
     database, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
     if err != nil {
         panic("failed to connect to database")
     }
+    database.AutoMigrate(&User{})
 }
 
 func Close() {
@@ -66,7 +70,10 @@ func CreateUser(username string, hash []byte, salt []byte) error {
 }
 
 func ReadUser(username string, body *User) error {
-    return TODO()
+    if err := database.First(body, "username = ?", username).Error; err != nil {
+        return err
+    }
+    return nil
 }
 
 func UpdateUser(username string, new_hash []byte, new_salt []byte) error {
